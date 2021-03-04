e4e3269a26bc3f9614703dec7a7736113f96f0e8
김대환 <e994301s@gmail.com>
2021-03-04 19:58:37 +0900

    3/4_storyboard init

---
+++ DodamDodam/DodamDodam/Setting/AlarmSettingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlarmSettingTableViewCell.swift
+//  DodamDodam
+//
+//  Created by 김대환 on 2021/03/04.
+//
+
+import UIKit
+
+class AlarmSettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DodamDodam/DodamDodam/Setting/ThemeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeViewController.swift
+//  DodamDodam
+//
+//  Created by 김대환 on 2021/03/04.
+//
+
+import UIKit
+
+class ThemeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

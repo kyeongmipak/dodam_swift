396f2a85bc932063057dea49239964628a2db224
Ria Song <donna_s@sookmyung.ac.kr>
2021-03-13 12:25:50 +0900

    21.03.13 _ Ria Song _ Monthly List Controller 추가

---
+++ DodamDodam/DodamDodam/MonthlyList/MonthlyListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MonthlyListTableViewCell.swift
+//  DodamDodam
+//
+//  Created by Ria Song on 2021/03/13.
+//
+
+import UIKit
+
+class MonthlyListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DodamDodam/DodamDodam/MonthlyList/MonthlyListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MonthlyListViewController.swift
+//  DodamDodam
+//
+//  Created by Ria Song on 2021/03/13.
+//
+
+import UIKit
+
+class MonthlyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
